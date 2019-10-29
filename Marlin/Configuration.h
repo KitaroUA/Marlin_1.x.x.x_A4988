4119b500ddc8488b344741bbf540ebe86de28319
Kitaro <Kitaro@CI0-300-6140W8.bank.gov.ua>
2019-10-29 09:26:00 +0200

    working 1.01

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -124,7 +124,7 @@
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
 //#define BAUDRATE 250000
-#define BAUDRATE 250000	// !!MY!!
+#define BAUDRATE 115200	// !!MY!!
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
